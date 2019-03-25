88f0bd3d5ced1d156edf5c719824c0b33848a85e
einaazod <aniedoza@gmail.com>
2019-03-26 04:25:49 +0800

    Migration - some fixes

---
+++ enay/database/migrations/2019_03_25_165646_create_healthcenter_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHealthcenterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('healthcenter', function (Blueprint $table) {
+            $table->string('healthcenterid')->primary()->default('HC');
+            $table->string('purokid');
+            $table->string('barangayid');
+            $table->string('cityid');
+            $table->timestamps();
+        });
+
+        Schema::table('healthcenter', function (Blueprint $table) {
+            $table->foreign('purokid')->references('purokid')->on('purok');
+            $table->foreign('barangayid')->references('barangayid')->on('barangay');
+            $table->foreign('cityid')->references('cityid')->on('city');
+        });
+
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('healthcenter');
+    }
+}
---
+++ enay/database/migrations/2019_03_25_165814_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('personnelid')->primary()->default('PER');
+            $table->string('fname');
+            $table->string('mname');
+            $table->string('lname');
+            $table->date('dob');
+            $table->string('phone');
+            $table->string('healthcenterid');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('healthcenterid')->references('healthcenterid')->on('healthcenter');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
